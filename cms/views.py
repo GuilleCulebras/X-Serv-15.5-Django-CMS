8104e4f5e202df9d984ef57f73189a44f68f7123
Guille <guicultal@gmail.com>
2017-03-28 20:07:11 +0200

    Ejercicio realizado

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from models import Pages
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def searchContent(request, name):
+	try:
+		page = Pages.objects.get(name=name)
+		respuesta = page.page
+	except Pages.DoesNotExist:
+		respuesta = "No existe"
+	return HttpResponse(respuesta)
+
+def createPage(request, name, page):
+	newPage = Pages(name=name,page=page)
+	newPage.save()
+
+	return HttpResponse(newPage.name + " guardada")
+
+def barra(request):
+	pages = Pages.objects.all()
+	respuesta = ""
+	for page in pages:
+		respuesta += page.name + " : " + page.page + "<br>"
+	return HttpResponse(respuesta)
+
+def error(request):
+	return HttpResponse("404 Not Found")
--- myproject/urls.py
+++ myproject/urls.py
@@ -6,5 +6,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^content/$', 'cms.views.barra'),
+    url(r'^content/create/(.*)/(.*)', 'cms.views.createPage'),
+    url(r'^content/(.*)', 'cms.views.searchContent'),
     url(r'^admin/', include(admin.site.urls)),
+	url(r'(.*)', 'cms.views.error'),
 )
